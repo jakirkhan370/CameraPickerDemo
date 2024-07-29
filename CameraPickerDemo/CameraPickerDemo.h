6fe7165acaf25152f1ba6c3f299b9a20c4af74f7
jakir_khan <jakir.khan@gingerwebs.co.in>
2024-07-29 13:00:54 +0530

    Initial Commit

---
+++ CameraPickerDemo/CameraPickerDemo.h
@@ -0,0 +1,18 @@
+//
+//  CameraPickerDemo.h
+//  CameraPickerDemo
+//
+//  Created by APPLE on 29/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CameraPickerDemo.
+FOUNDATION_EXPORT double CameraPickerDemoVersionNumber;
+
+//! Project version string for CameraPickerDemo.
+FOUNDATION_EXPORT const unsigned char CameraPickerDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraPickerDemo/PublicHeader.h>
+
+
